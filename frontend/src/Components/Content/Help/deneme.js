6fa62abf7d198d67ccfc7c52cdd019936a34c554
kAli <atemurtas97@gmail.com>
2021-04-22 22:28:39 +0300

    22.28

---
+++ frontend/src/Components/Content/Help/deneme.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class deneme extends Component {
+    render() {
+        return (
+            <div>
+                <h1>dfadsfsadffdfdfdfefeffdfefdfef</h1>
+            </div>
+        )
+    }
+}
