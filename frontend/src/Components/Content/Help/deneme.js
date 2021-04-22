2c88b013ef6d4ed25721dfda11fb927c2f9a9701
kAli <atemurtas97@gmail.com>
2021-04-22 22:45:24 +0300

    ol artık

---
+++ frontend/src/Components/Content/Help/deneme.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class deneme extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ali temurtaşasdfasdfadsfdddd</h1>
+            </div>
+        )
+    }
+}
