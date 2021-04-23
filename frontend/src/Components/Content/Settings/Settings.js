88beb8f3470d82f47959fdc007a3b58dbc6d99c1
deno <deno>
2021-04-24 02:42:39 +0300

    24/04/2021 - 02:42

--- frontend/src/Components/Content/Settings/Settings.js
+++ frontend/src/Components/Content/Settings/Settings.js
@@ -1,29 +1,36 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
-import Tab1 from './Settings Tabs/Tab1'
+import WorkingHoursTab from './Settings Tabs/WorkingHoursTab'
+import NightShiftTab from './Settings Tabs/NightShiftTab';
+import PlanningTab from './Settings Tabs/PlanningTab';
+import BreakTab from './Settings Tabs/BreakTab';
 
 function Settings() {
-    const [key, setKey] = useState('home');
+    const [key, setKey] = useState('WorkingHours');
 
     return (
         <Tabs
             id="controlled-tab-example"
+            defaultActiveKey={"WorkingHours"}
             activeKey={key}
             onSelect={(k) => setKey(k)}
+            
         >
-            <Tab eventKey="home" title="Home">
-                <Tab1 />
+            <Tab eventKey="WorkingHours" title="Working Hours">
+                <WorkingHoursTab />
             </Tab>
-            <Tab eventKey="profile" title="Profile">
-                <Tab1 />
+            <Tab eventKey="NightShift" title="Night Shift">
+                <NightShiftTab />
             </Tab>
-            <Tab eventKey="contact" title="Contact" disabled>
-                <Tab1 />
+            <Tab eventKey="Break" title="Break Limits">
+                <BreakTab />
+            </Tab>
+            <Tab eventKey="Planning" title="Planning">
+                <PlanningTab />
             </Tab>
         </Tabs>
     );
 }
-
 
 export default Settings;
